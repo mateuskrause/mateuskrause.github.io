7377b6633c76d3b08409a02ab793d09b5363b488
mateuskrause <mateus.ke@outlook.com>
2020-07-13 17:48:02 -0300

    generate-file started working

--- scripts/generate-articles.sh
+++ scripts/generate-articles.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 header=../page-elements/header.html
 footer=../page-elements/footer.html
+
+#circular por todos os arquivos
 content=../articles-src/Lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit.html
-title=abcd
-output=../articles/$title.html
-touch $output
-#part1=$(cat $header | cut -d '%' -f 2)
-title=$(head -n 1 $content | cut -c 4- | rev | cut -c 6- | rev)
+
+url_title=$(basename $content .html)
+output=../articles-test/$url_title.html
+
+title=$(head -n 1 $content | cut -c 5- | rev | cut -c 6- | rev)
 title+=" - Penguin's Daydreams"
-part1=$(cat $header | sed "s/<title><\/title>/<title>$title<\/title>/g")
-echo $part1
-#IFS='%' read -a array <<< $(cat $header)
-#echo $part1
-#echo "${array[0]}"
-#cat $(cat $header | sed "s/<title><\/title>/<title>$title<\/title>/g") >> $output
-#cat $header >> $output
-#cat $content >> $output
-#cat $footer >> $output
+
+new_header=$(cat $header | sed "s/<title><\/title>/<title>$title<\/title>/g")
+
+if [ -f $output ] ; then
+	cat /dev/null > $output
+else
+	touch $output
+fi
+
+echo $new_header >> $output
+cat $content >> $output
+cat $footer >> $output
