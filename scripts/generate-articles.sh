586b42b7ed77aae23baa3ee430beaeda0ef4c643
mateuskrause <mateus.ke@outlook.com>
2020-07-15 15:10:32 -0300

    Broken links fixed

--- scripts/generate-articles.sh
+++ scripts/generate-articles.sh
@@ -6,6 +6,8 @@ footer=../page-elements/footer.html
 sed1="s/href=\"style.css\"/href=\"..\/style.css\"/g"
 sed2="s/href=\"images\/site\/favicon.png\"/href=\"..\/images\/site\/favicon.png\"/g"
 sed3="s/src=\"images\/site\/icon.png\"/src=\"..\/images\/site\/icon.png\"/g"
+sed4="s/href=\"contact.html\"/href=\"..\/contact.html\"/g"
+sed5="s/href=\"index.html\"/href=\"..\/index.html\"/g" 
 
 for content in $(ls ../articles-src -1 | grep .html)
 do
@@ -16,7 +18,7 @@ do
 	title=$(head -n 1 $content | cut -c 5- | rev | cut -c 6- | rev)
 	title+=" - Penguin's Daydreams"
 
-	new_header=$(cat $header | sed "s/<title><\/title>/<title>$title<\/title>/g" | sed $sed1 | sed $sed2 | sed $sed3)
+	new_header=$(cat $header | sed "s/<title><\/title>/<title>$title<\/title>/g" | sed $sed1 | sed $sed2 | sed $sed3 | sed $sed4 | sed $sed5)
 
 	if [ -f $output ] ; then
 		cat /dev/null > $output
